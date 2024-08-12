9722521c7466035240ac93ee7e07d93573194440
Lucas Reis <luctreis@gmail.com>
2024-08-12 18:52:51 -0300

    AEDs-II TP-I Exercício 10 Resolvido

---
+++ segundoPeriodo/AEDS-II/TPs/tp1/ex010/Main.java
@@ -0,0 +1,31 @@
+import java.util.*;
+
+public class Main
+{
+
+	public static boolean ehPalindromo (String word,int esq, int dir)
+	{
+		if(esq > dir) 
+			return true;
+		else if(word.charAt(esq) != word.charAt(dir))
+			return false;
+		else
+			return ehPalindromo (word, esq+1, dir-1);
+	}
+
+	public static void main (String[] args)
+	{
+		Scanner sc = new Scanner (System.in);
+
+		String word = sc.nextLine();
+
+		if(ehPalindromo (word,0,word.length() - 1))
+		{
+			System.out.println("SIM");
+		}
+		else
+		{
+			System.out.println("NÃO");
+		}
+	}
+}
