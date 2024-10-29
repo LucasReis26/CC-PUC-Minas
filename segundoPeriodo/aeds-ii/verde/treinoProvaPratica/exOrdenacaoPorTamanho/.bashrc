f2ab962533709f51f0f7a9c2b752c1fb4958d356
Lucas Reis <luctreis@gmail.com>
2024-10-29 14:45:10 -0300

    exOrdenacaoPorTamanho concluido

---
+++ segundoPeriodo/aeds-ii/verde/treinoProvaPratica/exOrdenacaoPorTamanho/.bashrc
@@ -0,0 +1,1 @@
+alias pop-update='~/pop-update'
--- segundoPeriodo/aeds-ii/verde/treinoProvaPratica/exOrdenacaoPorTamanho/main.c
+++ segundoPeriodo/aeds-ii/verde/treinoProvaPratica/exOrdenacaoPorTamanho/main.c
@@ -2,6 +2,13 @@
 #include <stdlib.h>
 #include <string.h>
 
+void swapArray(int *array, int n1, int n2)
+{
+	int temp = *(array + n1);
+	*(array + n1) = *(array + n2);
+	*(array + n2) = temp;
+}
+
 int separaPalavras(char *word,char ***arrayString)
 {
 	int contador = 1;
@@ -41,21 +48,57 @@ int separaPalavras(char *word,char ***arrayString)
 	return contador;
 }
 
-int* organizaPalavras(int **arrayString)
+int* ordenaPalavras(char **arrayString, int tam)
 {
-	int *intArray = (int *)malloc(sizeof(int));
+	int *intArray = (int *)malloc(tam * sizeof(int));
+
+	for(int i = 0; i < tam; i++)
+	{
+		*(intArray + i) = i;
+	}
+
+	for(int i = 1; i < tam ; i++)
+	{
+
+		int j = i - 1;	
+
+		while(j >= 0 && strlen(*(arrayString + *(intArray + j))) < strlen(*(arrayString + *(intArray + j + 1))) )
+		{
+			swapArray(intArray,j,j+1);
+			j--;
+		}
+	}
+
+	return intArray;
 }
 
 int main()
 {
 	char **arrayString;
-	char *string = (char *)malloc(51*sizeof(char));
+	char *string = (char *)malloc(51 * sizeof(char));
+	int numPalavras;
 
-	scanf(" %[^\n]",string);
+	scanf("%d",&numPalavras);
 
-	int contaPalavras = separaPalavras(string,&arrayString);
-	
-	int *organizaPalavras = organizaPalavras(arrayString);	
+	for(int j = 0; j < numPalavras; j++)
+	{
+		scanf(" %[^\n]",string);
 
+		int contaPalavras = separaPalavras(string,&arrayString);
+		
+		int *organizaPalavras = ordenaPalavras(arrayString, contaPalavras);	
+
+		for(int i = 0; i < contaPalavras; i++)
+		{
+			if(i != (contaPalavras - 1))
+				printf("%s ",*(arrayString + *(organizaPalavras + i)));
+			else
+				printf("%s",*(arrayString + *(organizaPalavras + i)));
+		}
+		printf("\n");
+	}
+
+	free(string);
+	free(arrayString);
 	return 0;
 }
